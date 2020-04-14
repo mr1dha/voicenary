f9987bdda1e086756fa4d04f4ef48042a92a8c49
mr1dha <muhammadridha11@gmail.com>
2020-04-14 12:00:17 +0700

    Membuat pengecekan hasil recognition dengan soal

---
+++ unity-sdk-3.1.0/Examples/QuestionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuestionController : MonoBehaviour{
+
+	private string[] questions = {
+		"apple", "book","school","pencil","publication", 
+	};
+	private int currentQuestion;
+
+	public QuestionController(){
+		currentQuestion = 0;
+	}
+
+	public int CurrentQuestion{
+		get{ return currentQuestion;}
+		set{ currentQuestion = value;}
+	}
+
+	public string Question{
+		get{
+			if(currentQuestion < questions.Length){
+				return questions[currentQuestion];
+			}else{
+				return null;
+			}
+		}
+	}
+}
